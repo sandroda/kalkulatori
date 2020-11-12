86a25095c198d5c557dfa942539e75fbe13ce761
sandroda <sandro.davitaia.1@btu.edu.ge>
2020-11-12 12:57:54 +0400

    first commit

---
+++ app/src/main/java/com/example/kalkulatori/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var resultTextView: TextView
+
+    private var operand: Double = 0.0
+    private var operation: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+
+    }
+
+    fun numberClick(view: View) {
+
+        if (view is TextView) {
+
+            val number: String = view.text.toString()
+            var result: String = resultTextView.text.toString()
+
+            if (result == "0") {
+                result = ""
+            }
+
+            resultTextView.text = result + number
+
+        }
+
+    }
+
+    fun operationClick(view: View){
+
+        if (view is TextView) {
+
+            if (!TextUtils.isEmpty(resultTextView.text)) {
+
+                operand = resultTextView.text.toString().toDouble()
+            }
+
+            resultTextView.text = ""
+
+            operation = view.text.toString()
+
+        }
+
+    }
+
+    fun equalsClick(view: View){
+
+        val  secOperandText = resultTextView.text.toString()
+        var secOperand: Double = 0.0
+
+        if (!TextUtils.isEmpty(secOperandText)){
+            secOperand = secOperandText.toDouble()
+
+        }
+
+        when (operation){
+            "+" -> resultTextView.text = (operand + secOperand).toString()
+            "-" -> resultTextView.text = (operand - secOperand).toString()
+            "*" -> resultTextView.text = (operand * secOperand).toString()
+            "/" -> resultTextView.text = (operand / secOperand).toString()
+            "√" -> resultTextView.text = (operand.pow(0.5)).toString()
+            "%" -> resultTextView.text = (operand * (secOperand/100)).toString()
+        }
+
+    }
+    fun clearClick(view: View){
+        resultTextView.text = ""
+    }
+    fun backspace(view: View){
+        var b = resultTextView.text.toString()
+        if(!TextUtils.isEmpty(b)) {
+            resultTextView.text = b.substring(0, b.length - 1)
+        }
+    }
+    fun wertili(view: View){
+        if (!resultTextView.text.toString().contains(".")){
+            var c =resultTextView.text.toString()
+            c += "."
+            resultTextView.text = c
+        }
+    }
+
+}
